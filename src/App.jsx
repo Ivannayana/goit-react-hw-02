c52ecf1eb7cb8a369c38411f8ce7e5ac2549a6d7
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-09-30 17:46:36 +0300

    second commit dz two

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,37 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
-function App() {
+const App = () => {
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
   return (
     <>
       <Description />
-      <Options />
-      <Feedback />
+      <Options updateFeedback={updateFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} />
+      ) : (
+        <Notification message="No feedback given yet" />
+      )}
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Feedback = ({ feedback }) => {
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <div>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Positive Feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
