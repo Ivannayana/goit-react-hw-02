3a8ebfcddcb1b030eadcceca9b403b00e8f309dd
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-09-30 15:00:14 +0300

    first commit dz two

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Description from "./components/Description/Description";
+
+function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback />
+    </>
+  );
+}
+
+export default App;
